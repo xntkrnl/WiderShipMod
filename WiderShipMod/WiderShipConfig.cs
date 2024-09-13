46a2e250c214445f98c3ee944ba5716ea3496dae
xntkrnl <maminbors@gmail.com>
2024-09-13 21:46:48 +0300

    test to main

--- WiderShipMod/WiderShipConfig.cs
+++ WiderShipMod/WiderShipConfig.cs
@@ -8,25 +8,32 @@ namespace WiderShipMod
     {
         public static ConfigEntry<bool> enableLeftInnerWall;
         public static ConfigEntry<bool> enableLeftInnerWallSolidMode;
-        //public static ConfigEntry<bool> enableLeftUpgradeBuyable;
-        //public static ConfigEntry<bool> enableRightWall;
+        public static ConfigEntry<bool> enableRightInnerWall;
+        public static ConfigEntry<bool> enableRightInnerWallSolidMode;
 
         public static void Config(ConfigFile cfg)
         {
-            enableLeftInnerWall = cfg.Bind("General", "Enable Left Inner Wall", true,
+            enableLeftInnerWall = cfg.Bind("Left Part", "Enable Left Inner Wall", true,
                 "Enable this if you want to have a wall between the new ship part and the vanilla one.");
-            enableLeftInnerWallSolidMode = cfg.Bind("General", "Solid Left Inner Wall", false,
+            enableLeftInnerWallSolidMode = cfg.Bind("Left Part", "Solid Left Inner Wall", false,
                 "Enable if you want your wall to be solid. Doesn't work without Left Inner Wall enabled.");
-            //enableLeftUpgradeBuyable = cfg.Bind("General", "Enable Upgrade Mode", false,
-            //    "WIP - DOES NOTHING.\nEnable this if you want to buy a new ship part.");
+
+            enableRightInnerWall = cfg.Bind("Right Part", "Enable Right Inner Wall", true,
+                "Enable this if you want to have a wall between the new ship part and the vanilla one.");
+            enableRightInnerWallSolidMode = cfg.Bind("Right Part", "Solid Right Inner Wall", false,
+                "Enable if you want your wall to be solid. Doesn't work without Left Inner Wall enabled.");
 
             var boolLeftInnerWall = new BoolCheckBoxConfigItem(enableLeftInnerWall);
             var boolLeftInnerWallSolidMode = new BoolCheckBoxConfigItem(enableLeftInnerWallSolidMode);
-            //var boolLeftUpgradeBuyable = new BoolCheckBoxConfigItem(enableLeftUpgradeBuyable);
+
+            var boolRightInnerWall = new BoolCheckBoxConfigItem(enableRightInnerWall);
+            var boolRightInnerWallSolidMode = new BoolCheckBoxConfigItem(enableRightInnerWallSolidMode);
 
             LethalConfigManager.AddConfigItem(boolLeftInnerWall);
             LethalConfigManager.AddConfigItem(boolLeftInnerWallSolidMode);
-            //LethalConfigManager.AddConfigItem(boolLeftUpgradeBuyable);
+
+            LethalConfigManager.AddConfigItem(boolRightInnerWall);
+            LethalConfigManager.AddConfigItem(boolRightInnerWallSolidMode);
         }
     }
 }

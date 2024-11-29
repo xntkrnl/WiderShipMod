abc9f87ae09106daa1b8e23f3d1013c07703ffe2
xntkrnl <maminbors@gmail.com>
2024-11-29 15:22:39 +0300

    1.3.10

--- WiderShipMod/WiderShipPlugin.cs
+++ WiderShipMod/WiderShipPlugin.cs
@@ -20,7 +20,7 @@ namespace WiderShipMod
         // Mod Details
         private const string modGUID = "mborsh.WiderShipMod";
         private const string modName = "WiderShipMod";
-        private const string modVersion = "1.3.9";
+        private const string modVersion = "1.3.10";
 
         private readonly Harmony harmony = new Harmony(modGUID);
 

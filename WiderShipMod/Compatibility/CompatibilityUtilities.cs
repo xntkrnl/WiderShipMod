71eb8336d6c2233e16c8ec533d2b74b5402c6836
xntkrnl <maminbors@gmail.com>
2025-02-16 17:21:51 +0300

    Version Check (for ShipWindows 2.x.x or Beta)

---
+++ WiderShipMod/Compatibility/CompatibilityUtilities.cs
@@ -0,0 +1,15 @@
+﻿using BepInEx.Bootstrap;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WiderShipMod.Compatibility
+{
+    internal static class CompatibilityUtilities
+    {
+        internal static bool IsInstalledVersionGreaterOrEquallThanNeeded(string guid, Version version) => Chainloader.PluginInfos.TryGetValue(guid, out var info) && info.Metadata.Version >= version;
+    }
+}
